dd0fab7e5d138c9e33c6e3504c43b67fdcb31810
TONY <os@MacBookPro.lan>
2025-02-16 22:05:17 -0600

    set up my pseudocode

---
+++ DoubleNode.java
@@ -0,0 +1,32 @@
+//Creates Class for DoubleNode 
+public class DoubleNode {
+    //Declare instance variables
+        //Stores the element data
+
+        //Reference to the nest node in the list
+        
+        //Reference to the previus node in the list 
+
+
+    //Creates class constructor
+        //Pre-conditon(s): one, and type double as initial data.
+        //Post-condition(s): Initialize the element data with the given inital data, and the other two instance variables as null.
+
+    //Creates getter method
+        //Pre-condition(s): none. 
+        //Post-condition(s): returns the element data of the double node.
+
+    //Creates setter method
+
+    //Overrides the toString() method and returns Current node object state
+
+    //Creates a new node object after the current one
+
+    //Removes the node after the current one
+
+    //Returns an int that represents the length of the LinkedList
+
+    //Shifts to the left of the list by a given number of steps
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+//Create Main class for testing the DoubleNode
+
+    //Initializes the node
+
+    //Add nodes after
+
+    //Print the list of hodes
+
+    //Tests the Getters and Setters
+
+    //Tests the removeNodeAfter
+
+    //Tests the listLength
+
+    //Test the rewind
